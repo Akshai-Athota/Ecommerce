57995876b0c96dd97cfada0052f73f7fb6ece635
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-06-28 21:26:01 +0530

    authenication feature added

--- src/main/java/com/akshai/Ecommerce/api/Controllers/auth/AuthenticationController.java
+++ src/main/java/com/akshai/Ecommerce/api/Controllers/auth/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.akshai.Ecommerce.api.Controllers.auth;
 
 import com.akshai.Ecommerce.Service.UserService;
+import com.akshai.Ecommerce.api.Models.LoginBody;
+import com.akshai.Ecommerce.api.Models.LoginResponse;
 import com.akshai.Ecommerce.api.Models.RegistrationBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,15 +17,26 @@ import javax.validation.Valid;
 @RequestMapping("/auth")
 public class AuthenticationController {
     @Autowired
-    private UserService registerservice;
+    private UserService userservice;
 
     @PostMapping("/register")
     public ResponseEntity registerUser(@Valid @RequestBody RegistrationBody registeruser){
         try {
-            registerservice.registerUserDetails(registeruser);
+            userservice.registerUserDetails(registeruser);
             return ResponseEntity.ok().build();
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenicateUser(@RequestBody LoginBody loginbody){
+        String jwt=userservice.authenticateUser(loginbody);
+        if(jwt == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        LoginResponse loginresponse=new LoginResponse();
+        loginresponse.setJwt(jwt);
+        return ResponseEntity.ok(loginresponse);
     }
 }
