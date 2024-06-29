8436cb01cca86ea687b34fe3ab9b259fe3806b70
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-06-29 12:57:20 +0530

    endpoints secured added

--- src/main/java/com/akshai/Ecommerce/api/Security/WebSecurityConfig.java
+++ src/main/java/com/akshai/Ecommerce/api/Security/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.akshai.Ecommerce.api.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 /**
  * Configuration of the security on endpoints.
@@ -11,6 +13,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
 
+
+    @Autowired
+    JWTRequestFilter jwtRequestFilter;
     /**
      * Filter chain to configure security.
      * @param http The security object.
@@ -19,9 +24,11 @@ public class WebSecurityConfig {
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //TODO: Proper authentication.
         http.csrf().disable().cors().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        http.authorizeHttpRequests()
+                .requestMatchers("/product","/auth/login","/auth/register").permitAll()
+                .anyRequest().permitAll();
         return http.build();
     }
 
